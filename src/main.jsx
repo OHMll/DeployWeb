14a53f0cfc199df5f5e67a0cfb7e36a159375473
OHMll <napadechkaewnok@gmail.com>
2025-05-09 01:26:40 +0700

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import "./index.css";
 import Menubar from "./components/menubar";  // ตรวจสอบ path ว่าตรงกับโครงสร้างโปรเจคของคุณ
 import Homepage from "./page/Home";
